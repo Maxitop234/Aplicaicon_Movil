699984fea571693eb8b138afb3ae76b97c9e6879
Korossuh <davidrubilar0106@gmail.com>
2024-11-15 18:34:47 -0300

    Funcionalidad de Botones del Main activity para redirigir a donde debe

--- app/src/main/java/com/example/evaluacion3/MainActivity.java
+++ app/src/main/java/com/example/evaluacion3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.evaluacion3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd, btnCrud, btnVer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Boton para poder ir a la vista Agregar Remedios
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentadd = new Intent(MainActivity.this,AgregarRemedio.class);
+                startActivities(intentadd);
+            }
+        });
+
+        //Boton para poder ir a la vista Modificar Remedios
+        btnCrud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentcrud = new Intent(MainActivity.this, CRUDRemedios.class);
+                startActivities(intentcrud);
+            }
+        });
+
+        //Boton para poder ir a la vista Ver Remedios
+        btnVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentver = new Intent(MainActivity.this, ObtenerRemedios.class);
+                startActivities(intentver);
+            }
         });
     }
 }
