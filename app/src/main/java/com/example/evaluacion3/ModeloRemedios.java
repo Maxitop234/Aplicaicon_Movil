39138be4b42436ac3bec36560e91a66a62165f5c
Korossuh <davidrubilar0106@gmail.com>
2024-11-15 17:58:30 -0300

    Creación de Modelo para la base de datos

--- app/src/main/java/com/example/evaluacion3/ModeloRemedios.java
+++ app/src/main/java/com/example/evaluacion3/ModeloRemedios.java
@@ -1,4 +1,61 @@
 package com.example.evaluacion3;
 
-public class ModeloRemedios {
+import java.io.Serializable;
+
+public class ModeloRemedios implements Serializable {
+
+    private String nombre,cantidad,fechavencimiento,mg,categoria,descripcion;
+    private int id;
+    //Para ID
+    public int getId() {
+        return id;
+    }
+
+    //Para nombre
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Para Cantidad
+    public String getCantidad() {
+        return cantidad;
+    }
+    public void setName(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    //Para Fecha de Vencimiento
+    public String getFechaVencimiento() {
+        return fechavencimiento;
+    }
+    public void setName(String fechavencimiento) {
+        this.fechavencimiento = fechavencimiento;
+    }
+
+    //Para Miligramos
+    public String getMg() {
+        return mg;
+    }
+    public void setMg(String mg) {
+        this.mg = mg;
+    }
+
+    //Para Categoria
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    //Para Descripcion
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
